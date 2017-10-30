e07d5290da7af0369e531a1fdf2fa29d203f48f4
jnoack <jnoack@yieldmo.com>
2017-10-30 15:49:13 -0400

    update typings.

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,11 @@
-// Type definitions for YM_SDK (web-formats) Project: ts-sdk
+
+// Type definitions for YM_SDK (web-formats)
+// Project: ts-sdk
+
+/**
+ * The ts-sdk is exposed as a global variable 'YM_SDK' in the format's window
+ */
+export as namespace YM_SDK;
 
 /**
  * Public methods and properties exposed for web-formats
@@ -10,21 +17,39 @@
  * @param placementId
  * @param clicktrackers
  */
-export function addClickTrackers(placementId : string, clicktrackers : string[]) : void;
+export function addClickTrackers(placementId: string, clicktrackers: string[]): void;
 
 /**
  * Legacy no-op method, still being called by format 30 (Map)
  *
  * @param _placementContainer html element containing the format's iframey
  */
-export function appendAdChoices(_placementContainer : HTMLElement) : void;
+export function appendAdChoices(_placementContainer: HTMLElement): void;
 
 /**
  * Adds click event handlers to all CTA anchor tags within a format.
  *
  * @param documentContext The format's document
  */
-export function attachClickEventHandlers(documentContext : Document) : void;
+export function attachClickEventHandlers(documentContext: Document): void;
+
+/**
+ * Allows a format to attach to a visibility event.
+ *
+ * @param placementWindow
+ * @param listener
+ * @param percentage percentage to trigger at.
+ * @param rising Listen to rising.
+ * @param keepAlive
+ */
+export function attachVisibilityEvent(placementWindow: PlacementWindow, listener: Function, percentage: number, rising: boolean, keepAlive?: boolean): boolean;
+
+/**
+ * Allows a format to remove a visibility event.
+ *
+ * @param listener
+ */
+export function detachFormatEventListener(placementWindow: PlacementWindow, eventName: string, listener: Function): void;
 
 /**
  * Adds an event listener through the YM_SDK object, for events which need to be proxied through the sdk
@@ -34,7 +59,7 @@ export function attachClickEventHandlers(documentContext : Document) : void;
  * @param listener function to fire when event is triggered
  * @returns true if successfully subscribed to the event, false otherwise
  */
-export function attachFormatEventListener(placementWindow : PlacementWindow, eventName : string, listener : Function) : boolean;
+export function attachFormatEventListener(placementWindow: PlacementWindow, eventName: string, listener: Function): boolean;
 
 /**
  * Cleanly removes a listener for an event that was proxied through the YM_SDK
@@ -43,7 +68,7 @@ export function attachFormatEventListener(placementWindow : PlacementWindow, eve
  * @param eventName type of event
  * @param listener the callback that was registered for the event
  */
-export function detachFormatEventListener(placementWindow : PlacementWindow, eventName : string, listener : Function) : void;
+export function detachFormatEventListener(placementWindow: PlacementWindow, eventName: string, listener: Function): void;
 
 /**
  * Indicates whether an event is supported by the current environment
@@ -51,19 +76,19 @@ export function detachFormatEventListener(placementWindow : PlacementWindow, eve
  * @param eventName type of event
  * @returns true if supported, false otherwise
  */
-export function doesSupportEvent(eventName : string) : boolean;
+export function doesSupportEvent(eventName: string): boolean;
 
 /**
  * Returns the topmost window we have access to in the current environment
  *
  * @returns the topmost window we can access, or null if we do not have an environment manager
  */
-export function environmentTopWindow() : Window | null;
+export function environmentTopWindow(): Window | null;
 
 /**
  * Logging static utility class
  */
-export const FormatLog : Log;
+export const FormatLog: Log;
 
 /**
  * Fetches the bounding client rectangle of the given node, representing its position on the page,
@@ -72,7 +97,7 @@ export const FormatLog : Log;
  * @param node whose bounding client rect we want
  * @returns a bounding client rect
  */
-export function getBoundingClientRect(node : HTMLElement) : BoundingClientRect;
+export function getBoundingClientRect(node: HTMLElement): BoundingClientRect;
 
 /**
  * Invokes the successCallback and provides geolocation if geolocation is available, otherwise if a failureCallback
@@ -81,7 +106,7 @@ export function getBoundingClientRect(node : HTMLElement) : BoundingClientRect;
  * @param successCallback function to call in case of success
  * @param failureCallback? optional function to call in case of failure
  */
-export function getClientGeolocation(successCallback : Function, failureCallback?: PositionErrorCallback) : void;
+export function getClientGeolocation(successCallback: Function, failureCallback?: PositionErrorCallback): void;
 
 /**
  * Returns a ScrollEventMessage with the element's boundingClientRect and its scroll position.
@@ -89,42 +114,42 @@ export function getClientGeolocation(successCallback : Function, failureCallback
  * @param formatIframe the format's iframe
  * @returns returns a ScrollEventMessage or null in rare instances where positioning info is unavailable
  */
-export function getCurrentFormatPosition(formatIframe : HTMLIFrameElement) : ScrollEventMessage | null;
+export function getCurrentFormatPosition(formatIframe: HTMLIFrameElement): ScrollEventMessage | null;
 
 /**
  * Determines if we are inside an Amp environment
  *
  * @returns true if Amp, false otherwise
  */
-export function isAMP() : boolean;
+export function isAMP(): boolean;
 
 /**
  * Determines if we are running on an Android device
  *
  * @returns true if Android, false otherwise
  */
-export function isAndroid() : boolean;
+export function isAndroid(): boolean;
 
 /**
  * Determines if we are inside a Dfp environment
  *
  * @returns true if Dfp, false otherwise
  */
-export function isDFP() : boolean;
+export function isDFP(): boolean;
 
 /**
  * Determines if we are running on an iOS device
  *
  * @returns true if iOS, false otherwise
  */
-export function isIOS() : boolean;
+export function isIOS(): boolean;
 
 /**
  * Prepares format iframe and parent container iframes to allow full screen mode
  *
  * @param formatIframe the format's iframe
  */
-export function requestFullScreenMode(formatIframe : HTMLIFrameElement) : void;
+export function requestFullScreenMode(formatIframe: HTMLIFrameElement): void;
 
 /**
  * Resizes our placement based on the current size of the content inside the format's iframe
@@ -132,14 +157,14 @@ export function requestFullScreenMode(formatIframe : HTMLIFrameElement) : void;
  * @param formatIframe the format's iframe
  * @returns true if successfully resized, false otherwise
  */
-export function resetDimensions(formatIframe : HTMLIFrameElement) : boolean;
+export function resetDimensions(formatIframe: HTMLIFrameElement): boolean;
 
 /**
  * Returns the top <body> tag of the current environment's topmost accessible window, or the current document's <head> tag as a fallback
  *
  * @returns top accessible body or head element in the current environment
  */
-export function topElem() : HTMLBodyElement | HTMLHeadElement;
+export function topElem(): HTMLBodyElement | HTMLHeadElement;
 
 /**
  * Reports an event to the ad server.
@@ -150,24 +175,25 @@ export function topElem() : HTMLBodyElement | HTMLHeadElement;
  * @param formatWindow the window of the placement that owns the event
  * @param creativeIds? send creative Ids with the event. If an event in the result of interaction with specific creatives, send an array of strings.
  * Otherwise, true sends all creative ids.
- */
-export function trackEv(type : string, startTime : string | number, eventData : any, formatWindow : PlacementWindow, creativeIds?: boolean | string[]) : void;
+*/
+export function trackEv(type: string, startTime: string | number, eventData: any, formatWindow: PlacementWindow,
+  creativeIds?: boolean | string[]): void;
 
 /**
  * Notifies the ad-server of an error being recorded.
  *
  * @param message error message
  * @param formatWindow the format's window
- * @param type error type
+ * @param type the type of error, of type YieldmoError
  */
-export function trackError(message : string, formatWindow : PlacementWindow, type?: string) : void;
+export function trackError(message: string, formatWindow: PlacementWindow, type?: string): void;
 
 /**
  * @returns supported features of the environment.
  * Smooth scroll indicates that we receive frequent position updates,
  * Full screen indicates full screen videos are available.
  */
-export function availableEnvironmentFeatures() : AvailableFeatures;
+export function availableEnvironmentFeatures(): AvailableFeatures;
 
 /**
  * Clears a placement.
@@ -178,46 +204,45 @@ export function availableEnvironmentFeatures() : AvailableFeatures;
  *
  * @param placementWindow the format's window
  */
-export function clearPlacement(placementWindow : PlacementWindow) : void;
+export function clearPlacement(placementWindow: PlacementWindow): void;
 
 /**
  * Type representing properties available to access within the format's window
  */
 export interface PlacementWindow extends Window {
-  YM_SDK : any;
-  YMHandlebars : any;
+  YM_SDK: any;
+  YMHandlebars: any;
 }
 
 /**
  * Supported features of the environment returned by YM_SDK.availableEnvironmentFeatures
  */
 export interface AvailableFeatures {
-  fullScreen : boolean;
-  scroll : boolean;
-  smoothScroll : boolean;
+  fullScreen: boolean;
+  smoothScroll: boolean;
 }
 
 /**
  * BoundingClientRect type containing positioning information returned by YM_SDK.getBoundingClientRect
  */
 export interface BoundingClientRect {
-  bottom : number;
-  height : number;
-  left : number;
-  right : number;
-  top : number;
-  width : number;
+  bottom: number;
+  height: number;
+  left: number;
+  right: number;
+  top: number;
+  width: number;
 }
 
 /**
  * ScrollEventMessage containing positioning and viewport information and returned by YM_SDK.getCurrentFormatPosition
  */
 export interface ScrollEventMessage {
-  boundingFormatRect : BoundingClientRect | null;
-  x : number;
-  y : number;
-  screenHeight : number;
-  screenWidth : number;
+  boundingFormatRect: BoundingClientRect | null;
+  x: number;
+  y: number;
+  screenHeight: number;
+  screenWidth: number;
 }
 
 /**
@@ -229,42 +254,42 @@ export interface Log {
    * @static
    * @returns The current log level
    */
-  getLogLevel() : LogLevel;
+  getLogLevel(): LogLevel;
 
   /**
    * Log a potential problem
    * @static
    * @param logArgs arguments to log
    */
-  warn(...logArgs : Object[]) : void;
+  warn(...logArgs: Object[]): void;
 
   /**
    * Log a serious error
    * @static
    * @param logArgs arguments to log
    */
-  error(...logArgs : Object[]) : void;
+  error(...logArgs: Object[]): void;
 
   /**
    * Log an informational message
    * @static
    * @param logArgs arguments to log
    */
-  info(...logArgs : Object[]) : void;
+  info(...logArgs: Object[]): void;
 
   /**
    * Log a message useful only for debugging
    * @static
    * @param logArgs arguments to log
    */
-  debug(...logArgs : Object[]) : void;
+  debug(...logArgs: Object[]): void;
 
   /**
    * Set amount of log messages
    * @static
    * @param logLevel log messages at level and above
    */
-  setLevel(logLevel : LogLevel) : void;
+  setLevel(logLevel: LogLevel): void;
 }
 
 /**
@@ -278,8 +303,3 @@ export enum LogLevel {
   Info, // also log informational messages
   Debug, // log many debugging messages
 }
-
-/**
- * The ts-sdk is exposed as a global variable 'YM_SDK' in the format's window
- */
-export as namespace YM_SDK;
